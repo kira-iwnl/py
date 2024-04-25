2a5da8a460bfe0c5703b3de99a7ae9ef3542098f
kira-iwnl <98198218+kira-iwnl@users.noreply.github.com>
2024-04-25 18:43:37 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,18 +1,13 @@
-def is_prime(n):
-    if n <= 1:
+def is_prime(num):
+    if num < 2:
         return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
             return False
-        i += 6
     return True
 
+# Print prime numbers between 1 and 100
 print("Prime numbers between 1 and 100:")
-for num in range(1, 101):
-    if is_prime(num):
-        print(num, end=" ")
+for i in range(1, 101):
+    if is_prime(i):
+        print(i, end=" ")
